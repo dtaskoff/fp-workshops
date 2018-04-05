7557a94e3f42610e96aacb52850ce1dafe89b12e
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-04-05 21:51:49 +0300

    Write three versions of the factorial function

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+
+factorial :: Word -> Word
+factorial n
+  | n < 2     = 1
+  | otherwise = n * factorial (n - 1)
+
+factorial' :: Word -> Word
+factorial' 0 = 1
+factorial' 1 = 1
+factorial' n = n * factorial' (n - 1)
+
+factorial'' :: Word -> Word
+factorial'' n =
+  case n of
+    0 -> 1
+    1 -> 1
+    _ -> n * factorial'' (n - 1)
