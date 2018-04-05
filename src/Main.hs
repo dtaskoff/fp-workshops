4c0c76d37e2165273573243341e4aef8a6635952
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-04-05 22:14:54 +0300

    Add subtraction and multiplication to our arithmetic operations

--- src/Main.hs
+++ src/Main.hs
@@ -62,23 +62,31 @@ skipSpaces s = s
 
 data Expr = Val Int
           | Add Expr Expr
+          | Sub Expr Expr
+          | Mul Expr Expr
   deriving Show
 
 parse :: String -> (Maybe Expr, [String])
 parse s = parse' (words' s)
 
 parse' :: [String] -> (Maybe Expr, [String])
-parse' ("+":cs) =
+parse' (op:cs) | op `member` ["+", "-", "*"] =
   let (lexpr, rest) = parse' cs
       (rexpr, rest') = parse' rest
   in  case (lexpr, rexpr) of
-        (Just l, Just r) -> (Just (Add l r), rest')
-        _ -> (Nothing, "+":cs)
+        (Just l, Just r) -> (opOp op l r, rest')
+        _ -> (Nothing, op:cs)
 parse' (n:cs) =
   case readMaybe n of
     Just n' -> (Just (Val n'), cs)
     Nothing -> (Nothing, n:cs)
 parse' other = (Nothing, other)
+
+opOp :: String -> Expr -> Expr -> Maybe Expr
+opOp "+" l r = Just (Add l r)
+opOp "-" l r = Just (Sub l r)
+opOp "*" l r = Just (Mul l r)
+opOp _ _ _ = Nothing
 
 evaluate :: String -> Maybe Int
 evaluate s =
@@ -89,3 +97,5 @@ evaluate s =
 evaluate' :: Expr -> Int
 evaluate' (Val n) = n
 evaluate' (Add lexpr rexpr) = evaluate' lexpr + evaluate' rexpr
+evaluate' (Sub lexpr rexpr) = evaluate' lexpr - evaluate' rexpr
+evaluate' (Mul lexpr rexpr) = evaluate' lexpr * evaluate' rexpr
